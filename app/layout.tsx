d263a73448c8a086c5e4f06e4abacea35d501e6e
azoz nabil <139650225+Azouz678@users.noreply.github.com>
2026-02-25 06:17:57 +0300

    Full dashboard management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,40 @@
-// import { ThemeProvider } from "@/components/theme-provider";
-// import type { Metadata } from "next";
-// import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import "./globals.css";
 
-// export const metadata: Metadata = {
-//   title: "Electros Store",
-//   description: "Electronics showroom",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{ children: React.ReactNode }>) {
-//   return (
-//     <html lang="ar" dir="rtl" suppressHydrationWarning>
-//       <body>
-//         {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem> */}
-//          <ThemeProvider>
-//           {children}
-//         </ThemeProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
+export const metadata: Metadata = {
+  title: "Electros Store",
+  description: "Electronics showroom",
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ar" dir="rtl" suppressHydrationWarning>
       <body>
-        {/* <ThemeProvider> */}
+        {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem> */}
+         {/* <ThemeProvider> */}
           {children}
         {/* </ThemeProvider> */}
       </body>
     </html>
   );
 }
+
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="ar" dir="rtl" suppressHydrationWarning>
+//       <body>
+//         {/* <ThemeProvider> */}
+//           {children}
+//         {/* </ThemeProvider> */}
+//       </body>
+//     </html>
+//   );
+// }
