c94bea86ee62c651af5fcdb96e9057bf405127e2
azoz nabil <139650225+Azouz678@users.noreply.github.com>
2026-02-26 02:04:46 +0300

    Full dashboard management

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -6,10 +6,10 @@ export default function ClientLayout({
   children: React.ReactNode
 }) {
   return (
-    <ThemeProvider storageKey="client-theme" defaultTheme="light">
+    // <ThemeProvider storageKey="client-theme" defaultTheme="light">
       <div className="min-h-screen bg-white dark:bg-slate-900 transition-colors duration-500">
         {children}
       </div>
-    </ThemeProvider>
+    // </ThemeProvider>
   )
 }
